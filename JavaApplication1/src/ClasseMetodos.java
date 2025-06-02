b4e3ae1be962d70c118cae10c896f35b0e754bc1
brenda <127804558+brendasantana04@users.noreply.github.com>
2025-06-02 20:52:40 -0300

    Add files via upload
    quickvote

--- JavaApplication1/src/ClasseMetodos.java
+++ JavaApplication1/src/ClasseMetodos.java
@@ -1,12 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.io.*;
+import javax.swing.*;
 
-/**
- *
- * @author FATEC ZONA LESTE
- */
 public class ClasseMetodos {
+    static void Main (String [] args ) {
+        
+    }
+
+    static void cadastraEleito () {
+
+        Eleitor e = new Eleitor();
+        
+
+        // repetir 3 vezes para 3 candidatos
+        for (int cta = 0; cta < 3; cta ++) {
+            e.NmrEleitor = Integer.parseInt(JOptionPane.showInputDialog("Insira o número do eleitor: "));
+            e.NmEleitor = JOptionPane.showInputDialog("Insira o nome do eleitor: ");
+            e.Sesao = Integer.parseInt(JOptionPane.showInputDialog("Insira a sessão do eleitor: "));
+            JOptionPane.showMessageDialog(null, "Eleitor Cadastrado!!!");
+        } 
+        
+        JOptionPane.showMessageDialog(null, "Eleitores Cadastrados com sucesso!!!");
+            
+        
+    }
+    
+
+
+
     
 }
--- JavaApplication1/src/Menu_Principal.java
+++ JavaApplication1/src/Menu_Principal.java
@@ -3,35 +3,37 @@ import javax.swing.*;
 
 class Menu_Principal { //Classe Principal 
         public static void main  ( String[] arg){
-            Eleitor [] eleitor = new Eleitor [10];
-            Eleicao [] eleicao = new Eleicao [10];
+            Eleitor [] eleitor = new Eleitor [3];
+            Eleicao [] eleicao = new Eleicao [7];
+
+            Eleitor e = new Eleitor();
             ClasseMetodos m  = new ClasseMetodos();
             
          
-                for (int i=0; i<10;i++){
+                for (int i=0; i< 3;i++){
              eleitor [i] = new Eleitor ();    
         }
-                for (int j=0; j <10; j++){
+                for (int j=0; j < 7; j++){
              eleicao [j] = new Eleicao ();
         }
                 
      int opc = 0;
      while(opc !=9){
-          opc =Integer.parseInt(JOptionPane.showInputDialog("1 - Cadastra Eleitor\n      "
-             + "                                       2 - Cadastra Votação 1 e 2\n"
-             + "                                        3 - Agrupa Apuração\n"
-             + "                                         4 - Menu Estatística\n"
-             + "                                           9 - FIM   "));
+          opc = Integer.parseInt(JOptionPane.showInputDialog("1 - Cadastra Eleitor\n"
+             + "2 - Cadastra Votação 1 e 2\n"
+             + "3 - Agrupa Apuração\n"
+             + "4 - Menu Estatística\n"
+             + "9 - FIM   "));
       
         switch (opc){
-             case 1: eleitor = m.cadastraEleito(eleitor);
+             case 1: m.cadastraEleito();
                  break;
-            case 2: cadastraVotacao();
-                break;
-             case 3: eleicao = m.apuracao(eleicao);
-                break;
-            case 4: menuEstatistica();
-                break;
+            // case 2: cadastraVotacao();
+            //     break;
+            //  case 3: eleicao = m.apuracao(eleicao);
+            //     break;
+            // case 4: menuEstatistica();
+            //     break;
      }  
         }
          
